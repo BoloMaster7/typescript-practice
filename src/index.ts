f171b89b1350a955719b60345106940393fffead
Pawel Godula <bolomaster7@gmail.com>
2023-05-03 15:52:18 +0200

    consola fix

--- src/index.ts
+++ src/index.ts
@@ -46,14 +46,16 @@ const startApp = () => {
       case Action.Quit:
         Message.showColorized(MessageVariant.Info, "Bye bye!");
         return;
+      
     }
-
-    startApp();
   });
 }
 
+startApp();
+
+
 class Message {
-  constructor(private content: string) { }
+  constructor( private content: string) { }
 
    show() {
       console.log(this.content);
@@ -84,6 +86,49 @@ class Message {
           consola.info(text);
       }
   }
+}
+
+enum MessageVariant {
+  Success = "success",
+  Error = "error",
+  Info = "info"
+}
+
+interface User {
+  name: string;
+  age: number;
+}
+
+
+class UsersData implements User {
+
+  name: any;
+  age: any;
+  data: User[] = [];
+
+  public showAll() {
+      Message.showColorized(MessageVariant.Info, "Users data")
+      this.data ? (console.table(this.data)) : (console.log("No data"));
+  }
+
+  public add(user: User) {
+      if (user.name.length > 0 && user.age > 0) {
+          this.data.push(user);
+          Message.showColorized(MessageVariant.Success, "User has been successfully added!");
+      } else {
+          Message.showColorized(MessageVariant.Error, "Wrong data!");
+      }
+  }
+
+  public remove(name: string) {
+      if (this.data.find((user) => user.name === name)) {
+          this.data = this.data.filter((user) => user.name !== name);
+          Message.showColorized(MessageVariant.Success, "User has been successfully removed!");
+      } else {
+          Message.showColorized(MessageVariant.Error, "User not found!");
+      }
+  }
+
 }
 
 
